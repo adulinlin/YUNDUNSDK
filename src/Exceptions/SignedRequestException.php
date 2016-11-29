1350d6a255cb3697e4d98df86efa0caa7552ac58
jason-gao <3048789891@qq.com>
2016-11-28 20:14:13 -0800

    adjust the directory structure

---
+++ src/Exceptions/SignedRequestException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Desc: SignedRequestException
+ * Created by PhpStorm.
+ * User: <gaolu@yundun.com>
+ * Date: 2016/11/25 10:47
+ */
+namespace YunDunSdk\Exceptions;
+
+class SignedRequestException extends YunDunSdkException{
+
+}
